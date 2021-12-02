a3f466e29c95d7830dabb9061ebca0fed7f185d9
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-02 18:21:44 +0100

    Add info about reference

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -2,6 +2,8 @@ import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  // Validamos el valor que sea opcional
+  // Y que contenga un valor de los disponibles en el Enum
   @IsOptional()
   @IsEnum(TaskStatus, {
     message: 'Debes de añadir un valor adecuado',
